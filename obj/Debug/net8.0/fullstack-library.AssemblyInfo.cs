4469d256a33d9d272fcd7d86e7deb2c2bb34e4b1
turkerkiv <kivturker@gmail.com>
2024-07-29 00:03:47 +0300

    Add empty entity classes from uml diagram

--- obj/Debug/net8.0/fullstack-library.AssemblyInfo.cs
+++ obj/Debug/net8.0/fullstack-library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fullstack-library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad63ef3f24c8c1dacae5424ea12befa7ca5e68e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990fb9c28ace9525e960146127eb858a51a37fe9")]
 [assembly: System.Reflection.AssemblyProductAttribute("fullstack-library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fullstack-library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
