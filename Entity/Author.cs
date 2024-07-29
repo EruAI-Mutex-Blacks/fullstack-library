6d1add38c4734b18ebd80476ffb87b48ca72a2eb
Fatihcagdas100 <154072241+Fatihcagdas100@users.noreply.github.com>
2024-07-29 20:50:10 +0300

    empty methods

--- Entity/Author.cs
+++ Entity/Author.cs
@@ -7,6 +7,17 @@ namespace fullstack_library.Entity
 {
     public class Author : User
     {
-        
+        public void writeBook()
+        {
+            //sitem üzerinden
+        }
+        public void uploadBook()
+        {
+            //PDF olarak
+        }
+        public void requestBookAddition()
+        {
+            //Ekleme talebi
+        }
     }
 }
