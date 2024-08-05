27510abccd5a628785334f69cfa291685d8c84e8
turkerkiv <kivturker@gmail.com>
2024-08-05 18:06:50 +0300

    update navbar and links on it

--- LibraryApp.ReactFrontEnd/src/full_pages/Login.jsx
+++ LibraryApp.ReactFrontEnd/src/full_pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
                         <input id="password" className="form-control" type="password" />
                     </div>
                     <div className="mb-3 px-1">
-                        <p className="fs-8">Are you not signed up? <Link className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" to="#">Create an account</Link></p>
+                        <p className="fs-8">Are you not signed up? <Link className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" to="/Register">Create an account</Link></p>
                     </div>
                     <button type="submit" className="text-white btn bg-custom-secondary">Login</button>
                 </form>
