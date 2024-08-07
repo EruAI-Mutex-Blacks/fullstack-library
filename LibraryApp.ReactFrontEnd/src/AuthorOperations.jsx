b0c4419732f2029d72048312ba6bfd44f3e20da4
turkerkiv <kivturker@gmail.com>
2024-08-07 20:44:13 +0300

    create parent card object to eliminite recursive codes

--- LibraryApp.ReactFrontEnd/src/AuthorOperations.jsx
+++ LibraryApp.ReactFrontEnd/src/AuthorOperations.jsx
@@ -1,20 +1,17 @@
-
+import OperationsCard from "./OperationsCard"
 
 function AuthorOperations() {
+
+    const items = (
+        <li className="">
+            <button className="btn bg-custom-primary" href="#">See pending book creation requests</button>
+        </li>
+    );
+
     return (
-        <div className="text-center d-flex flex-column bg-custom-secondary rounded p-4 my-5">
-            <div className="text-white mb-3 pt-1">
-                <h3>Author Operations</h3>
-                <hr />
-            </div>
-            <div className="flex-fill d-flex flex-column ">
-                <ul className="list-inline flex-fill d-flex flex-column  align-items-center">
-                    <li className="">
-                        <button className="btn bg-custom-primary" href="#">See pending book creation requests</button>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <>
+            <OperationsCard title="Author operations" items={items} />
+        </>
     )
 }
 
