d855fe11cf5aad675302045d2a2dab21602f6316
turkerkiv <kivturker@gmail.com>
2024-08-11 01:48:05 +0300

    make search book page with seed js datas

--- LibraryApp.ReactFrontEnd/src/full_pages/OperationsPages/GeneralOperationsPage.jsx
+++ LibraryApp.ReactFrontEnd/src/full_pages/OperationsPages/GeneralOperationsPage.jsx
@@ -8,7 +8,7 @@ function GeneralOperationsPage(props) {
             <div className="w-25 text-break">
                 {props.leftPanel}
             </div>
-            <div className=" flex-fill m-5 p-4 border border-dark rounded">
+            <div className=" flex-fill m-5 rounded">
                 {props.rightPanel}
             </div>
         </div>
--- LibraryApp.ReactFrontEnd/src/full_pages/OperationsPages/SearchBookOP.jsx
+++ LibraryApp.ReactFrontEnd/src/full_pages/OperationsPages/SearchBookOP.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import BookOperationsCard from "../../Components/OperationsCards/BookOperationsCard"
 import GeneralOperationsPage from "./GeneralOperationsPage"
 
@@ -7,9 +7,61 @@ function SearchBookOP() {
     const location = useLocation();
     const bookQuery = new URLSearchParams(location.search).get('book');
 
-    const rightPanel = "";
+    const seedBook = {
+        id: 1,
+        title: "Book 1",
+        publishDate: `${new Date().getDate()}-${new Date().getUTCMonth()}-${new Date().getFullYear()}`,
+        isBorrowed: false
+    }
 
-    return (<GeneralOperationsPage leftPanel={(<BookOperationsCard />)} rightPanel={bookQuery} />)
+    const seedBooks = [];
+    seedBooks.push(seedBook);
+    seedBooks.push(seedBook);
+    seedBooks.push(seedBook);
+    seedBooks.push(seedBook);
+    seedBooks.push(seedBook);
+    seedBooks.push(seedBook);
+
+    function handleBorrowClick(book) {
+        if (book.isBorrowed) {
+            window.alert("already borrowed");
+        }
+        else {
+            window.alert("can be borrowed");
+        }
+    }
+
+    const rightPanel = (
+        <table className="table table-light table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Publish Date</th>
+                    <th>Is Borrowed</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {seedBooks.map(b => (
+                    <tr>
+                        <td>{b.id}</td>
+                        <td>{b.title}</td>
+                        <td>{b.publishDate}</td>
+                        <td>{b.isBorrowed ? "Yes" : "No"}</td>
+                        <td>
+                            <ul className="list-inline d-flex justify-content-around">
+                                <li><Link onClick={() => {handleBorrowClick(b)}} className="btn btn-success">Borrow</Link></li>
+                                <li><Link onClick={() => {handleBorrowClick(b)}} className="btn btn-success">Borrow</Link></li>
+                            </ul>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+
+    return (<GeneralOperationsPage leftPanel={(<BookOperationsCard />)} rightPanel={rightPanel} />)
 }
 
 export default SearchBookOP
