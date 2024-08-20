463900ee4cc6379f4d2b0c004726a8413332a200
turkerkiv <kivturker@gmail.com>
2024-08-20 18:58:11 +0300

    create register endpoint

--- LibraryApp.WebAPI/Controllers/AccountController.cs
+++ LibraryApp.WebAPI/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using fullstack_library.DTO;
 using LibraryApp.Data.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using LibraryApp.Data.Entity;
 
 namespace fullstack_library.Controllers
 {
@@ -19,15 +20,33 @@ namespace fullstack_library.Controllers
             _userRepo = userRepo;
         }
 
+        //TODO make every action method async Task<>
+
         [HttpPost("Login")]
         public IActionResult Login(LoginDTO loginDTO)
         {
             var user = _userRepo.GetUsers().FirstOrDefault(u => u.Username == loginDTO.Username);
-            if(user == null) return NotFound("Username not found");
-            if(user.Password != loginDTO.Password) return StatusCode(401, "Password is incorrect");
+            if (user == null) return NotFound("Username not found");
+            if (user.Password != loginDTO.Password) return StatusCode(401, "Password is incorrect");
 
             //TODO return with jwt
             return Ok(user);
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            //TODO here we make user newUser first then a staff will approve or deny the req
+            _userRepo.CreateUser(new User
+            {
+                Name = registerDTO.Name,
+                Surname = registerDTO.Surname,
+                Username = registerDTO.Username,
+                Password = registerDTO.Password,
+                BirthDate = registerDTO.BirthDate,
+                Gender = registerDTO.Gender,
+            });
+            return Ok("User registered");
         }
     }
 }
