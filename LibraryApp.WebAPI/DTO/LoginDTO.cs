4733bcdd2f68f412bf6947aa601dba062ce1bdd2
turkerkiv <kivturker@gmail.com>
2024-08-20 19:01:40 +0300

    delete unnecessary confirming password at login

--- LibraryApp.WebAPI/DTO/LoginDTO.cs
+++ LibraryApp.WebAPI/DTO/LoginDTO.cs
@@ -9,6 +9,5 @@ namespace fullstack_library.DTO
     {
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
