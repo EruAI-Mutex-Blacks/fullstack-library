fe58e1f4203489995c1fff34aa568ea2161c3ad5
turkerkiv <kivturker@gmail.com>
2024-08-21 11:40:06 +0300

    add conditional components to the homepage

--- LibraryApp.ReactFrontEnd/src/full_pages/Home.jsx
+++ LibraryApp.ReactFrontEnd/src/full_pages/Home.jsx
@@ -4,28 +4,43 @@ import MessageOperationsCard from '../Components/OperationsCards/MessageOperatio
 import MemberOperationsCard from '../Components/OperationsCards/MemberOperationsCard.jsx';
 import StaffOperationsCard from '../Components/OperationsCards/StaffOperationsCard.jsx';
 import AuthorOperationsCard from '../Components/OperationsCards/AuthorOperationsCard.jsx';
+import { useUser } from '../AccountOperations/UserContext.jsx';
 
 function Home() {
+    const { user } = useUser();
+
     return (
         <div className='container'>
             <div className="row d-flex flex-row justify-content-around">
-                <div className="col-4">
-                    <BookOperationsCard/>
-                </div>
-                <div className="col-4">
-                    <MessageOperationsCard/>
-                </div>
-                <div className="col-4">
-                    <MemberOperationsCard/>
-                </div>
-                <div className="col-4">
-                    <StaffOperationsCard/>
-                </div>
-                <div className="col-4">
-                    <AuthorOperationsCard/>
-                </div>
+                {(["default", "staff", "manager"].includes(user.role)) && (
+                    <>
+                        <div className="col-4">
+                            <BookOperationsCard />
+                        </div>
+                        < div className="col-4">
+                            <MessageOperationsCard />
+                        </div>
+                    </>
+                )}
+                {
+                    (["staff", "manager"].includes(user.role)) && (
+                        < div className="col-4">
+                            <MemberOperationsCard />
+                        </div>
+                    )}
+                {
+                    (user.role === "manager") && (
+                        <>
+                            < div className="col-4">
+                                <StaffOperationsCard />
+                            </div>
+                            < div className="col-4">
+                                <AuthorOperationsCard />
+                            </div>
+                        </>
+                    )}
             </div>
-        </div>
+        </div >
     )
 }
 
