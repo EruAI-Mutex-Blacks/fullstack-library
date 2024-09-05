6466e08cfdde63822819ed95481f62bf2307052c
turkerkiv <kivturker@gmail.com>
2024-09-05 14:04:17 +0300

    fix settings' name's first letter to upper

--- LibraryApp.ReactFrontEnd/src/full_pages/Settings.jsx
+++ LibraryApp.ReactFrontEnd/src/full_pages/Settings.jsx
@@ -54,7 +54,7 @@ function Settings() {
             {settings?.map((s, index) => (
                 <div key={index} className="flex space-x-4 items-center">
                     <div className="mb-3 grow">
-                        <label className="text-gray-200 block font-medium mb-1" htmlFor={s.name}>{s.name.replaceAll("-", " ")}</label>
+                        <label className="text-gray-200 block font-medium mb-1" htmlFor={s.name}>{s.name.replaceAll("-", " ").replace(s.name[0], s.name[0].toUpperCase())}</label>
                         <input id={s.name} className="px-4 py-2 w-full bg-gray-700 text-gray-200 rounded border border-gray-600 focus:ring-blue-500 focus:ring-2 focus:border-blue-400 focus:outline-none hover:ring-2" type="number" step={0.1} defaultValue={s.newValue} onChange={e => { s.newValue = e.target.value }} />
                     </div>
                     <button className="border border-transparent inline-block rounded px-4 py-2 mt-4 bg-green-700 hover:bg-green-800 hover:ring-green-500 hover:ring-2 transition-all duration-100 text-white active:bg-green-900" onClick={() => { updateSetting(s) }}>Update</button>
