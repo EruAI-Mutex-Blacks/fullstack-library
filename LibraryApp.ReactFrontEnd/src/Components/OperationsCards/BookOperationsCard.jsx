4cdbde4f48f4beadcd7b918530e3a0af02f2383a
turkerkiv <kivturker@gmail.com>
2024-09-10 15:28:03 +0300

    change colors of home cards

--- LibraryApp.ReactFrontEnd/src/Components/OperationsCards/BookOperationsCard.jsx
+++ LibraryApp.ReactFrontEnd/src/Components/OperationsCards/BookOperationsCard.jsx
@@ -29,7 +29,7 @@ function BookOperationsCard({ onSearch }) {
         <>
             <li className="mb-4">
                 <div className="flex grow">
-                    <input type="text" className="px-4 py-3 text-sm w-full rounded-s ring-1 ring-text-light bg-primary-light focus:ring-2 focus:bg-primary-light/90 focus:ring-accent-dark outline-none hover:ring-accent hover:ring-2 text-text transition-all duration-100" placeholder="Book's name" onChange={handleInputChange} />
+                    <input type="text" className="px-4 py-3 text-sm w-full rounded-s bg-primary-light focus:ring-2 focus:bg-primary-light/90 focus:ring-accent-dark outline-none hover:ring-accent hover:ring-2 text-text transition-all duration-100" placeholder="Book's name" onChange={handleInputChange} />
                     <button className="text-xs rounded-e bg-accent inline-block px-5 py-1 hover:bg-accent-dark transition-all duration-100 active:bg-accent-dark/50 text-text font-semibold ease-in-out hover:ring-2 hover:ring-accent" onClick={handleSearchClick}>Search</button>
                 </div>
             </li>
--- LibraryApp.ReactFrontEnd/src/Components/OperationsCards/HomeGeneralOperationsCard.jsx
+++ LibraryApp.ReactFrontEnd/src/Components/OperationsCards/HomeGeneralOperationsCard.jsx
@@ -2,7 +2,7 @@
 function HomeGeneralOperationsCard(props) {
 
     return (
-        <div className="text-center h-full flex flex-col bg-primary-light/50 p-8 px-4 pb-16">
+        <div className="text-center h-full flex flex-col bg-primary-dark p-8 px-4 pb-16">
             <div className="text-text border-b mb-6">
                 <h3 className="text-2xl mb-2">{props.title}</h3>
             </div>
